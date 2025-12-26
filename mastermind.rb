890bb03991f3e8e91dda280c6fb6a991d4ce6356
Ash-x1 <ash91p@proton.me>
2025-12-26 20:27:32 +0100

    Add more logic to the game

--- mastermind.rb
+++ mastermind.rb
@@ -14,16 +14,44 @@ class Logic
     p col
   end
 
+  def valid_answer
+    loop do
+      p ' Please insert your guess: '
+      user_input = gets.chomp.downcase
+      return user_input if colors_list.include?(user_input)
+
+      p 'Please enter a valid answer from the list of colors above'
+    end
+  end
+
   def player_guess
-    guess = [gets]
-    i = 0
-    while i 
-    user_input = gets.chomp
-    p guess
+    guesses = []
+
+    until guesses.length == 4
+      guess = valid_answer
+
+      if guesses.include?(guess)
+        puts 'You have duplicated answers, no need to do that to avoid wasting your precious guesses'
+      else
+        guesses << guess
+      end
+    end
+
+    p guesses
+    guesses
+  end
+
+  def check_answer(secret, guess)
+    if secret == guess
+      p "CONGRATULATIONS!!! You have won the 'Mastermind' game"
+    else
+      p 'Sadly, you have lost but you can try again if you want'
+    end
   end
 end
 
 log = Logic.new
-# log.colors_list
-log.random_combinations
-log.player_guess
+
+secret = log.random_combinations
+guess  = log.player_guess
+log.check_answer(secret, guess)
